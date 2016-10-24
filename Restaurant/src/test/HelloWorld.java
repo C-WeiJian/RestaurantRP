b8cbe55ae1b4fd4ed42e01dc20ec679d8322bde1
icycoldx3 <adnakerh@gmail.com>
2016-10-24 15:58:55 +0800

    MEOW

--- Restaurant/src/test/HelloWorld.java
+++ Restaurant/src/test/HelloWorld.java
@@ -7,6 +7,9 @@ public class HelloWorld {
 		
 		System.out.print("Meow Meow Meow");
 		printrand();
+		
+		for(int i = 0; i<10;i++)
+			System.out.print("Meow Meow Meow");
 
 	}
 	public static void printrand(){
