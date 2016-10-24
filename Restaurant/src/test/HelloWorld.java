3d2e7d0cf88a17e02209b26a800e6c943a96bc00
wj <wj@10.27.46.87>
2016-10-24 15:59:09 +0800

    hihihi

--- Restaurant/src/test/HelloWorld.java
+++ Restaurant/src/test/HelloWorld.java
@@ -4,13 +4,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("hello world!!! adna here");
-		
+		printrand();
 		System.out.print("Meow Meow Meow");
 		printrand();
+		
 
 	}
 	public static void printrand(){
 		double a = Math.random();
-		System.out.print(a);
+		System.out.println(a);
 	}
 }
