089870efc8d287d0b297cc48d7491d7decadbe15
wj <wj@10.27.46.87>
2016-10-24 15:55:57 +0800

    random

--- Restaurant/src/test/HelloWorld.java
+++ Restaurant/src/test/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("hello world!!! adna here");
+	
 
 	}
-
+	public void printrand(){
+		double a = Math.random();
+		System.out.print(a);
+	}
 }
